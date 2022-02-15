ac779f4a5602a58a21216d13f93eab9fe72b9fbe
Rafał Fijałkowski <rafalski76@tlen.pl>
2022-02-15 18:25:24 +0100

    Terraform projekt update

---
+++ projekt/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-0015a39e4b7c0966f"
+  instance_type = "t2.micro"
+  
+  tags = {
+	Name = "RF-vm"
+  }
+}
